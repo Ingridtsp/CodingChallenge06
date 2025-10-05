5a32416d89c644efbf004ab978f760ebb65aa947
Ingridtsp <ingridnicole@usf.edu>
2025-10-05 13:27:56 -0400

    completed step 3: creating manager subclass extending employee

--- cc_6.js
+++ cc_6.js
@@ -9,3 +9,17 @@ describe(){
     return `${this.name} works in ${this.department}`;
 }
 }
+
+//Step 3: Create manager subclass extending Employee
+class Manager extends Employee {
+    constructor(name, department, teamSize){
+        super(name, department);
+        this.teamSize = teamSize;
+    }
+
+    //Override the describe() method
+describe(){
+    return`${this.name} manages a team of ${this.teamSize} people in the ${this.department} department`;
+
+}
+}
